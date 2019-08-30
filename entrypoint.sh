977e95925d66029553cfca16653e3850733d6573
apiloqbc <48203828+apiloqbc@users.noreply.github.com>
2019-08-30 16:06:18 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,30 @@
-#!/bin/sh -l
+#!/bin/bash
 
 set -e
 
-echo "==========Starting Maven Commands=========="
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+echo "Registering the ip"
 
-sh -c "mvn $*"
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
 
-echo "==========Finished Maven Commands=========="
+URI=https://api.github.com
+API_HEADER="Accept: application/vnd.github.v3+json"
+AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+
+HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
+HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
+
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO.git
+git config --global user.email \"$EMAIL\"
+git config --global user.name \"$GHUSER\"
+
+set -o xtrace
+
+git fetch origin master
+
+
+
+mvn release:prepare release:perform -B --settings .github.settings.xml 
