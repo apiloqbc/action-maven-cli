9c2078cc461e0216813751a5caf3b9e6fe08968f
Andrea, Pilo <apilo@cuebiq.com>
2019-09-03 00:07:53 +0200

    Added file settings to mvn command

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,5 +16,5 @@ git checkout master
 
 
 NEW_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout | sed -e "s/-SNAPSHOT//g")
-mvn versions:set -DnewVersion=$NEW_VERSION
-mvn deploy
+mvn versions:set -DnewVersion=$NEW_VERSION 
+mvn deploy -s .github.settings.xml
