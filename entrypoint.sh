66d3fda892930101dae169bbb42900b10472bfba
apiloqbc <48203828+apiloqbc@users.noreply.github.com>
2019-09-03 12:36:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,9 @@ set -e
 REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 echo "Registering the ip"
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Set the GITHUB_TOKEN env variable."
-	exit 1
-fi
+
 
 set -o xtrace
-
-git checkout master
-
 
 NEW_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout | sed -e "s/-SNAPSHOT//g")
 
